32f45e64de4f453301680ddaf470f6b7a8c8af14
Shepherd <kshe5782@740s-pl-112.local>
2019-02-07 08:37:44 -0700

    finished list.hpp

--- Model/Linear/List.hpp
+++ Model/Linear/List.hpp
@@ -9,7 +9,22 @@
 #ifndef List_hpp
 #define List_hpp
 
+#include "../Nodes/LinearNode.hpp"
 
-
+template <class Type>
+class List
+{
+protected:
+    int size;
+public:
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    
+    virtual int getSize() const = 0;
+    virtual LinerNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+    };
 
 #endif /* List_hpp */
