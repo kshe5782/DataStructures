54304083ca12705e01486cb332f4b12252d5be6b
Shepherd <kshe5782@740s-pl-112.local>
2019-02-07 08:24:40 -0700

    added list abstract class

---
+++ Model/Linear/List.hpp
@@ -0,0 +1,15 @@
+//
+//  List.h
+//  DataStructures
+//
+//  Created by Shepherd, Kayden on 2/7/19.
+//  Copyright © 2019 Ctec. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+
+
+
+#endif /* List_hpp */
