5533c89cabde030c7e156e87e07843d3c2a8dfc6
Shepherd <kshe5782@740s-pl-112.local>
2019-03-08 09:28:42 -0700

    Added graph class

---
+++ Diagrams/Graph.hpp
@@ -0,0 +1,72 @@
+//
+//  Graph.hpp
+//  DataStructures
+//
+//  Created by Shepherd, Kayden on 3/8/19.
+//  Copyright © 2019 Ctec. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 50;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+    
+public:
+    Graph();
+    
+    void addVertex(const Type& value);
+    
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    Type& operato [] (int vertex);
+    Typeoperator [] (int vertex) const;
+    int  size() const;
+    
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target)const;
+    std::set<int> neighbors(int vertex) const;
+    
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+}
+
+template <class Type>
+const int Graph<Type> :: MAXIMUM;
+
+template <class Type>
+Graph<Type> :: Graph()
+{
+    this->vertexCount = 0;
+}
+
+template <class Type>
+int Graph<Type> :: size() const
+{
+    return vertexCount;
+}
+
+
+
+
+#endif /* Graph_h */
